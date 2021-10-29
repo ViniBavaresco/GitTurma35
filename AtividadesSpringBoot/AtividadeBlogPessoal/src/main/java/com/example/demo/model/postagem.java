a2205d64cbf81e76b817f01c5c376e9639c70177
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-10-28 22:53:02 -0300

    Atividade BlogPessoal

---
+++ AtividadesSpringBoot/AtividadeBlogPessoal/src/main/java/com/example/demo/model/postagem.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity 
+@Table (name ="tb_postagem")
+public class postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String título;
+	
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return título;
+	}
+	public void setTitulo(String titulo) {
+		this.título = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+}
