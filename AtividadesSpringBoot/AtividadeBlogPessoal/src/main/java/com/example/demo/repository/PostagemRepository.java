f2fdc33c976ea55c119f500d3b631c6cdce5844d
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-11-01 17:04:29 -0300

    Up

--- AtividadesSpringBoot/AtividadeBlogPessoal/src/main/java/com/example/demo/repository/PostagemRepository.java
+++ AtividadesSpringBoot/AtividadeBlogPessoal/src/main/java/com/example/demo/repository/PostagemRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.Postagem;
