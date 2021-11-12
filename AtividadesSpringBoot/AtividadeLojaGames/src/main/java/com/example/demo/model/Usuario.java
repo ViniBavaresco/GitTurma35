e9aae403e73b6e89e5828cf764be820ea5632a5e
ViniBavaresco <91674067+ViniBavaresco@users.noreply.github.com>
2021-11-12 15:25:36 -0300

    Security Loja de Games

---
+++ AtividadesSpringBoot/AtividadeLojaGames/src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,63 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank(message = "Valor não pode ser nulo")
+	@Size(min = 5, max = 80, message = "O nome completo deve ter entre 5 e 80 caracteres")
+	private String nome;
+
+	@NotBlank(message = "Valor não pode ser nulo")
+	@Size(min = 5, max = 80, message = "O nome de usuário deve ter entre 5 e 80 caracteres")
+	private String usuario;
+
+	@NotBlank(message = "Valor não pode ser nulo")
+	@Size(min = 5, max = 80, message = "A senha deve ter entre 5 e 80 caracteres, com letras e números")
+	private String senha;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
--- AtividadesSpringBoot/AtividadeLojaGames/src/main/java/com/example/demo/repository/ProdutoRepository.java
+++ AtividadesSpringBoot/AtividadeLojaGames/src/main/java/com/example/demo/repository/ProdutoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.model.Produto;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-	public List<Produto> findAllByNomeContainingIgnoreCase (String nome);
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
 }
